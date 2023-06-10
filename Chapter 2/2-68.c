0e6f27bc909a4b407c6f65ea5d68bf47cfb3da32
Giam Chiu <giamchiu@gmail.com>
2023-06-11 00:35:10 +0800

    2-68

---
+++ Chapter 2/2-68.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int lower_one_mask(int n) {
+    int W = sizeof(int) * 8;
+    return (unsigned)-1 >> (W - n);
+}
+
+int main() {
+    printf("%.2d -> 0x%.8X\n", 6, lower_one_mask(6));
+    printf("%.2d -> 0x%.8X\n", 17, lower_one_mask(17));
+    printf("%.2d -> 0x%.8X\n", 32, lower_one_mask(32));
+    system("pause");
+    return 0;
+}
