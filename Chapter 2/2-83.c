a657cb032c7b131361a061e13bfedc92d898aad1
Giam Chiu <giamchiu@gmail.com>
2023-06-14 21:42:21 +0800

    2-83

---
+++ Chapter 2/2-83.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/*
+ * A 
+ * n=0.yyyy...
+ * n<<k = y.yyyy... = Y + n
+ * (n<<k) - n = Y
+ * n * (2^k-1) = Y
+ * n = Y / ((2^k)-1)
+ * 
+ * B
+ * (a)101
+ * 5/7
+ * (b)0110
+ * 6/15=2/5
+ * (c)010011
+ * 19/63
+ */
+int main() { 
+    system("pause");
+    return 0;
+}
