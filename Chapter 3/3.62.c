2a683da8463c7cf16a5bbfb610073ac36b9bc9c6
Giam Chiu <giamchiu@gmail.com>
2023-08-14 11:56:30 +0800

    3.62

---
+++ Chapter 3/3.62.c
@@ -0,0 +1,12 @@
+long switch3(long *p1, long *p2, mode_t action){ 
+    long result= O; 
+    switch(action) { 
+        case MODE_A: 
+        case MODE_B : 
+        case MODE_C : 
+        case MODE_D: 
+        case MODE_E:
+        default: 
+} 
+    return result; 
+}
